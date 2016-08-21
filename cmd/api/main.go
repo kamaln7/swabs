3300e6d3bb1383bf84982419c1c3affcd7bc254d
Kamal Nasser <hello@kamal.io>
2016-08-21 17:17:23 -0400

    switch to gorilla/mux, add two more routes

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,28 +3,46 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
 
 var db *sql.DB
 
+type Brand struct {
+	Name, Slug string
+}
+
+type Ink struct {
+	Brand      Brand
+	Name, Slug string
+}
+
+type InkDetailed struct {
+	Brand                  Brand
+	Name, Slug, URL, Donor string
+}
+
 func GetBrands(w http.ResponseWriter, r *http.Request) {
-	rows, err := db.Query("select distinct brand from inks order by brand asc")
+	rows, err := db.Query("select distinct brand, brand_slug from inks order by brand asc")
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
 	defer rows.Close()
 
-	var brands []string
+	var brands []Brand
 	for rows.Next() {
-		var brand string
-		err := rows.Scan(&brand)
+		var brand Brand
+		err := rows.Scan(&brand.Name, &brand.Slug)
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
+
 			return
 		}
 
@@ -32,6 +50,71 @@ func GetBrands(w http.ResponseWriter, r *http.Request) {
 	}
 
 	json.NewEncoder(w).Encode(brands)
+}
+
+func GetBrand(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	brandSlug := vars["brandSlug"]
+
+	rows, err := db.Query("select brand, brand_slug, name, name_slug from inks where brand_slug = ? order by name asc", brandSlug)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	var (
+		inks  []Ink
+		brand Brand
+		found bool = false
+	)
+	for rows.Next() {
+		found = true
+		var ink Ink
+		err := rows.Scan(&brand.Name, &brand.Slug, &ink.Name, &ink.Slug)
+		ink.Brand = brand
+
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		inks = append(inks, ink)
+	}
+
+	if !found {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(w, "[]")
+		return
+	}
+
+	json.NewEncoder(w).Encode(inks)
+}
+
+func GetInk(w http.ResponseWriter, r *http.Request) {
+	var (
+		ink   InkDetailed
+		brand Brand
+		vars  map[string]string = mux.Vars(r)
+	)
+
+	brand.Slug, ink.Slug = vars["brandSlug"], vars["inkSlug"]
+
+	err := db.QueryRow("select brand, name, url, donor from inks where brand_slug = ? and name_slug = ?", brand.Slug, ink.Slug).Scan(&brand.Name, &ink.Name, &ink.URL, &ink.Donor)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		return
+	}
+
+	ink.Brand = brand
+
+	json.NewEncoder(w).Encode(ink)
 }
 
 func main() {
@@ -42,8 +125,10 @@ func main() {
 	}
 	defer db.Close()
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/brands", GetBrands)
+	r := mux.NewRouter().PathPrefix("/v1/").Subrouter()
+	r.HandleFunc("/brands", GetBrands).Methods("GET")
+	r.HandleFunc("/brands/{brandSlug}", GetBrand).Methods("GET")
+	r.HandleFunc("/brands/{brandSlug}/inks/{inkSlug}", GetInk).Methods("GET")
 
 	address := os.Getenv("API_ADDR")
 	if address == "" {
@@ -51,5 +136,13 @@ func main() {
 	}
 
 	log.Printf("Listening on [%s]\n", address)
-	log.Fatal(http.ListenAndServe(address, mux))
+	srv := &http.Server{
+		Handler: r,
+		Addr:    address,
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
